02b7e627c0e41fe7b31b2b86f0fc7969a4b361da
Fredrik Johansson <fredrik@mumme.se>
2023-05-22 22:27:29 +0200

    Make serial usable

--- webroot/modules/serial.mjs
+++ webroot/modules/serial.mjs
@@ -9,44 +9,46 @@ class Serial {
     static onInitialized = () => {};
     static onConnected = () => {};
     static onDisconnected = () => {};
+    static rxCallback = () => {};
     outputStream = null;
-    
+
     // gemensam sänd function för Serial bluetooth eller USB
     async send(key, value) {
         const writer = this.outputStream.getWriter();
         writer.write(key + value + '\n');
         writer.releaseLock();
     }
-        
+
     static async init(messenger) {
-        if (this.initialized) 
-            return this.messenger("Web serial är redan initialiserad");
+        const _this = Serial.instance
+        if (_this?.initialized)
+            return _this.messenger("Web serial är redan initialiserad");
 
         let self = new Serial(); // create a new singleton
-        self.instance = self;
+        Serial.instance = self;
         self.messenger = messenger;
-        
+
         if ("serial" in navigator) {
             // fråga användare om vilken serieport, ställ in baudrate etc.
-            port = await navigator.serial.requestPort();
+            self.port = port = await navigator.serial.requestPort();
             await port.open({baudRate: 115200});
-            
+
             port.addEventListener('connnect', self.onConnected);
             port.addEventListener('disconnect', self.ondisconnected)
 
             const encoder = new TextEncoderStream();
             let outputDone = encoder.readable.pipeTo(port.writable);
-            this.outputStream = encoder.writable;
+            self.outputStream = encoder.writable;
 
-            this.initialized = outputDone && this.outputStream;
+            Serial.initialized = outputDone && this.outputStream;
             Serial.onInitialized();
-            
+            setTimeout(self._readFromPort.bind(self), 10);
         } else
             self.log("Web Serial är inte aktiverat. Måste aktiveras först");
 
         return port && port.writable;
     };
-    
+
     log(msg) {
         if (this.messenger)
             this.messenger.show(msg);
@@ -56,5 +58,31 @@ class Serial {
     static reset() {
         port = null;
         Serial.instance = null;
+    }
+
+    async _readFromPort(){
+        const enc = new TextDecoder();
+        while (this.port.readable) {
+            const reader = this.port.readable.getReader();
+            try {
+              while (true) {
+                const { value, done } = await reader.read();
+                if (done) {
+                  // |reader| has been canceled.
+                  break;
+                }
+                try {
+                    const str = enc.decode(value);
+                    Serial.rxCallback(str);
+                } catch(e) {
+                    console.error(e);
+                }
+              }
+            } catch (error) {
+              // Handle |error|...
+            } finally {
+              reader.releaseLock();
+            }
+          }
     }
 }
